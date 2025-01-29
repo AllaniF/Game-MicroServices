2a8d8227a399d8df6b10a6a1f9637b2e5e69019a
Timothe C <timothe.chiesi@etu.mines-ales.fr>
2025-01-29 11:19:27 +0100

    Initiated module for service 'Game-State'

---
+++ Game-State-Service/src/main/java/com/example/gamestateservice/GameStateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamestateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStateServiceApplication.class, args);
+    }
+
+}
