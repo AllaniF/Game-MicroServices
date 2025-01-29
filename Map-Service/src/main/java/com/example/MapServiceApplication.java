1b0b83b6e46ec189cdda60788c70e2608c61306f
Omayma El kasbaoui <omayma.el-kasbaoui@etu.mines-ales.fr>
2025-01-29 14:53:34 +0100

    feat : map service

--- Map-Service/src/main/java/com/example/MapServiceApplication.java
+++ Map-Service/src/main/java/com/example/MapServiceApplication.java
@@ -1,10 +1,10 @@
-package com.example.mapservice;
+package com.example; // Your package name
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MapServiceApplication {
+public class MapServiceApplication{ // Or YourProjectNameApplication
 
     public static void main(String[] args) {
         SpringApplication.run(MapServiceApplication.class, args);
