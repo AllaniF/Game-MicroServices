8a0d5b85c6fe6faa912481f0b5073fef8c3f314c
Omayma El kasbaoui <omayma.el-kasbaoui@etu.mines-ales.fr>
2025-01-31 09:49:54 +0100

    feat : update entity service to consume rabbitmq queue

---
+++ Entity-Service/src/main/java/com/example/HeroUpdateListener.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class HeroUpdateListener {
+
+    private final EntityService entityService;
+
+    public HeroUpdateListener(EntityService entityService) {
+        this.entityService = entityService;
+    }
+
+    @RabbitListener(queues = "update-hero-queue")
+    public void receiveHeroUpdate(Map<String, Object> heroData) {
+        System.out.println("Received hero update: " + heroData);
+
+        // Extract values from the map
+        Integer id = (Integer) heroData.get("id");
+        Integer level = (Integer) heroData.get("level");
+        Integer dunjonNb = (Integer) heroData.get("dunjonNb");
+        Integer maxHP = (Integer) heroData.get("maxHP");
+        Integer gold = (Integer) heroData.get("gold");
+        Integer atk = (Integer) heroData.get("atk");
+
+        // Update existing Entity instance or create a new one
+        Entity hero = new Entity();
+        hero.setId(id);
+        hero.setLevel(level);
+        hero.setDunjonNb(dunjonNb);
+        hero.setMaxHP(maxHP);
+        hero.setGold(gold);
+        hero.setAtk(atk);
+
+        // Save updated hero
+        entityService.updateHeros(id, hero);
+    }
+}
